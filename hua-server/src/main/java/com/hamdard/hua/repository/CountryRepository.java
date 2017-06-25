8335ba3144d5d4ee016da5704a7f0c48ac4f3282
chandab0 <chanda.b0@gmail.com>
2017-06-25 16:47:17 +0530

    Changed RestConfig and CountryRepository

--- hua-server/src/main/java/com/hamdard/hua/repository/CountryRepository.java
+++ hua-server/src/main/java/com/hamdard/hua/repository/CountryRepository.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.MarkerManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import com.hamdard.hua.model.Country;
 import com.hamdard.hua.model.Department;
@@ -24,6 +25,7 @@ import com.hamdard.hua.rowmapper.DepartmentRowMapper;
  * @author Biswajit
  *
  */
+@Component
 public class CountryRepository {
 	
 
