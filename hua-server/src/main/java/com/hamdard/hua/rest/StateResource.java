fde4c720c70d16054b4c10dcee8276aa1c232917
Saptarshi Basu <sapb2004@gmail.com>
2017-06-26 19:50:31 +0530

    All API permission data created and the resource field size in permission table changed to 100

--- hua-server/src/main/java/com/hamdard/hua/rest/StateResource.java
+++ hua-server/src/main/java/com/hamdard/hua/rest/StateResource.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,16 +18,13 @@ import org.springframework.stereotype.Component;
 
 import com.hamdard.hua.model.Message;
 import com.hamdard.hua.model.State;
-import com.hamdard.hua.privileges.Privilege;
-
 import com.hamdard.hua.repository.StateRepository;
-import com.hamdard.hua.security.Secured;
 
 /**
  * @author Biswajit
  *
  */
-
+@Component
 @Path("v1/state")
 public class StateResource {
 
@@ -41,14 +37,14 @@ public class StateResource {
 	@Path("/")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	// @Secured(Privilege.STATE_ALL_READ_CMD)
-	public Response getState(@QueryParam("countryId") @NotNull @Min(1) int countryId) throws Exception {
+	public Response getState(@QueryParam("countryId") @NotNull @Min(1)  int countryId) throws Exception {
 		List<State> states;
 		try {
 			states = stateRepository.getStateByCountryId(countryId);
 
 			/* If returned countryId doesn't return any values */
 			if (states.isEmpty()) {
-				logger.error("No state is found for countryId: {}", () -> countryId);
+				logger.error("No state is found for countryId: " + countryId);
 				return Response.status(404).entity(new Message("No state found for the given country.")).build();
 			}
 
@@ -60,7 +56,7 @@ public class StateResource {
 		/* Any other kind of exception */
 		catch (Exception ex) {
 			logger.error(ex.getMessage());
-			return Response.status(500).entity(new Message(ex.getMessage())).build();
+			return Response.status(500).entity(new Message("Internal Server Error")).build();
 		}
 
 	}
