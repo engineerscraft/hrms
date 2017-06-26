36e68b29b4f7eb6530f8c33e46e2423f39243fef
chandab0 <chanda.b0@gmail.com>
2017-06-26 15:29:19 +0530

    Added exception handling in StateResource

--- hua-server/src/main/java/com/hamdard/hua/rest/StateResource.java
+++ hua-server/src/main/java/com/hamdard/hua/rest/StateResource.java
@@ -2,15 +2,23 @@ package com.hamdard.hua.rest;
 
 import java.util.List;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.hamdard.hua.model.Message;
 import com.hamdard.hua.model.State;
 import com.hamdard.hua.privileges.Privilege;
 
@@ -24,6 +32,9 @@ import com.hamdard.hua.security.Secured;
 @Component
 @Path("v1/state")
 public class StateResource {
+
+	private static final Logger logger = LogManager.getLogger(StateResource.class);
+
 	@Autowired
 	StateRepository stateRepository;
 
@@ -31,12 +42,26 @@ public class StateResource {
 	@Path("/")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	// @Secured(Privilege.STATE_ALL_READ_CMD)
-	public List<State> getState(@QueryParam("countryId") int countryId) {
+	public Response getState(@QueryParam("countryId") @NotNull @Min(1)  int countryId) throws Exception {
+		List<State> states;
 		try {
+			states = stateRepository.getStateByCountryId(countryId);
 
-			return stateRepository.getStateByCountryId(countryId);
-		} catch (Exception e) {
-			throw e;
+			/* If returned countryId doesn't return any values */
+			if (states.isEmpty()) {
+				logger.error("No state is found for countryId: " + countryId);
+				return Response.status(404).entity(new Message("No state found for the given country.")).build();
+			}
+
+			/* If countryId return associated values */
+			else
+				return Response.status(200).entity(states).build();
+		}
+
+		/* Any other kind of exception */
+		catch (Exception ex) {
+			logger.error(ex.getMessage());
+			return Response.status(500).entity(new Message("Internal Server Error")).build();
 		}
 
 	}
