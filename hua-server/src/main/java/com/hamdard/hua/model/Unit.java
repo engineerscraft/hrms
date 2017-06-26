80bb9231fa6c578396fe9776d06ee4e85ef150c9
iSomdeb <samanta.som@gmail.com>
2017-06-26 19:05:28 +0530

    Fixes as per standards and Error Handling Response for Units

--- hua-server/src/main/java/com/hamdard/hua/model/Unit.java
+++ hua-server/src/main/java/com/hamdard/hua/model/Unit.java
@@ -1,104 +1,105 @@
 package com.hamdard.hua.model;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * @author Somdeb
  *
  */
 public class Unit {
-	private int unitId;
-	private String unitName;
-	@NotNull(message = "Organization ID cannot be empty")
-	private int orgId;
-	private String empIdPrefix;
-	private String empIdSeqName;
-	private String address;
+    private int unitId;
+    private String unitName;
+    private int orgId;
+    private String empIdPrefix;
+    private String empIdSeqName;
+    private String address;
 
-	/**
-	 * @return the unitId
-	 */
-	public int getUnitId() {
-		return unitId;
-	}
+    /**
+     * @return the unitId
+     */
+    public int getUnitId() {
+        return unitId;
+    }
 
-	/**
-	 * @param unitId
-	 *            the unitId to set
-	 */
-	public void setUnitId(int unitId) {
-		this.unitId = unitId;
-	}
-	
-	/**
-	 * @return the unitName
-	 */
-	public String getUnitName() {
-		return unitName;
-	}
+    /**
+     * @param unitId
+     *            the unitId to set
+     */
+    public void setUnitId(int unitId) {
+        this.unitId = unitId;
+    }
 
-	/**
-	 * @param unitName
-	 *            the unitName to set
-	 */
-	public void setUnitName(String unitName) {
-		this.unitName = unitName;
-	}
+    /**
+     * @return the unitName
+     */
+    public String getUnitName() {
+        return unitName;
+    }
 
-	/**
-	 * @return the orgId
-	 */
-	public int getOrgId() {
-		return orgId;
-	}
+    /**
+     * @param unitName
+     *            the unitName to set
+     */
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
 
-	/**
-	 * @param orgId the orgId to set
-	 */
-	public void setOrgId(int orgId) {
-		this.orgId = orgId;
-	}
+    /**
+     * @return the orgId
+     */
+    public int getOrgId() {
+        return orgId;
+    }
 
-	/**
-	 * @return the empIdPrefix
-	 */
-	public String getEmpIdPrefix() {
-		return empIdPrefix;
-	}
+    /**
+     * @param orgId the orgId to set
+     */
+    public void setOrgId(int orgId) {
+        this.orgId = orgId;
+    }
 
-	/**
-	 * @param empIdPrefix the empIdPrefix to set
-	 */
-	public void setEmpIdPrefix(String empIdPrefix) {
-		this.empIdPrefix = empIdPrefix;
-	}
+    /**
+     * @return the empIdPrefix
+     */
+    public String getEmpIdPrefix() {
+        return empIdPrefix;
+    }
 
-	/**
-	 * @return the empIdSeqName
-	 */
-	public String getEmpIdSeqName() {
-		return empIdSeqName;
-	}
+    /**
+     * @param empIdPrefix the empIdPrefix to set
+     */
+    public void setEmpIdPrefix(String empIdPrefix) {
+        this.empIdPrefix = empIdPrefix;
+    }
 
-	/**
-	 * @param empIdSeqName the empIdSeqName to set
-	 */
-	public void setEmpIdSeqName(String empIdSeqName) {
-		this.empIdSeqName = empIdSeqName;
-	}
+    /**
+     * @return the empIdSeqName
+     */
+    public String getEmpIdSeqName() {
+        return empIdSeqName;
+    }
 
-	/**
-	 * @return the address
-	 */
-	public String getAddress() {
-		return address;
-	}
+    /**
+     * @param empIdSeqName the empIdSeqName to set
+     */
+    public void setEmpIdSeqName(String empIdSeqName) {
+        this.empIdSeqName = empIdSeqName;
+    }
 
-	/**
-	 * @param address the address to set
-	 */
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Unit [unitId=" + unitId + ", unitName=" + unitName + ", orgId=" + orgId + ", empIdPrefix=" + empIdPrefix + ", empIdSeqName=" + empIdSeqName + ", address=" + address + "]";
+    }
 }
