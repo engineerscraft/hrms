2d4d7e7f8140ac15695c1c5309a12d085d3928b8
Saptarshi Basu <sapb2004@gmail.com>
2017-07-01 18:18:46 +0530

    new employee sub-resource management added

--- hua-server/src/main/java/com/hamdard/hua/rest/EmployeeResource.java
+++ hua-server/src/main/java/com/hamdard/hua/rest/EmployeeResource.java
@@ -31,7 +31,7 @@ import com.hamdard.hua.security.Secured;
  * Employee related service's endpoint!
  */
 
-@Path("/v1/employee")
+@Path("/v1/employee/management")
 public class EmployeeResource {
     
     private static final Logger logger = LogManager.getLogger(EmployeeResource.class);
