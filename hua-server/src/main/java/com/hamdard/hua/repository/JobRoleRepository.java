46ac9d90bd2ac1e922795f087bc64367849bc453
lookatchandra <lookatchandra@gmail.com>
2017-07-03 01:50:42 -0700

    Code for Salary and Optional Benefits by Job Role Id

--- hua-server/src/main/java/com/hamdard/hua/repository/JobRoleRepository.java
+++ hua-server/src/main/java/com/hamdard/hua/repository/JobRoleRepository.java
@@ -12,25 +12,58 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.hamdard.hua.model.JobRole;
+import com.hamdard.hua.model.Salary;
 import com.hamdard.hua.rowmapper.JobRoleRowMapper;
+import com.hamdard.hua.rowmapper.SalaryRowMapper;
 
 @Component
 public class JobRoleRepository {
-    private static final Logger logger = LogManager.getLogger(JobRoleRepository.class);
-    private static final Marker sqlMarker = MarkerManager.getMarker("SQL");
+	private static final Logger logger = LogManager
+			.getLogger(JobRoleRepository.class);
+	private static final Marker sqlMarker = MarkerManager.getMarker("SQL");
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    @Value("${sql.jobrole.get.byOrgId}")
-    private String jobroleGetByOrgIdSql;
+	@Value("${sql.jobrole.get.byOrgId}")
+	private String jobroleGetByOrgIdSql;
 
-    public List<JobRole> getJobRolesByOrgId(int orgId) {
+	@Value("${sql.salary.get.byJobRoleId}")
+	private String salaryGetByJobRoleIdSql;
 
-        logger.info(sqlMarker, jobroleGetByOrgIdSql);
-        logger.info(sqlMarker, "Params {}", () -> orgId);
-        List<JobRole> jobRoles = (List<JobRole>) jdbcTemplate.query(jobroleGetByOrgIdSql, new Object[] { orgId }, new JobRoleRowMapper());
-        logger.debug("Retrieved job roles: {}", () -> jobRoles);
-        return jobRoles;
-    }
+	@Value("${sql.optbenefits.get.byJobRoleId}")
+	private String OptBenefitsGetByJobRoleIdSql;
+
+	public List<JobRole> getJobRolesByOrgId(int orgId) {
+
+		logger.info(sqlMarker, jobroleGetByOrgIdSql);
+		logger.info(sqlMarker, "Params {}", () -> orgId);
+		List<JobRole> jobRoles = (List<JobRole>) jdbcTemplate.query(
+				jobroleGetByOrgIdSql, new Object[] { orgId },
+				new JobRoleRowMapper());
+		logger.debug("Retrieved job roles: {}", () -> jobRoles);
+		return jobRoles;
+	}
+
+	public List<Salary> getSalaryByJobRoleId(long jobRoleId) {
+
+		logger.info(sqlMarker, salaryGetByJobRoleIdSql);
+		logger.info(sqlMarker, "Params {}", () -> jobRoleId);
+		List<Salary> salary = (List<Salary>) jdbcTemplate.query(
+				salaryGetByJobRoleIdSql, new Object[] { jobRoleId },
+				new SalaryRowMapper());
+		logger.debug("Retrieved salary : {}", () -> salary);
+		return salary;
+	}
+
+	public List<Salary> getOptionalBenefitsByJobRoleId(long jobRoleId) {
+
+		logger.info(sqlMarker, OptBenefitsGetByJobRoleIdSql);
+		logger.info(sqlMarker, "Params {}", () -> jobRoleId);
+		List<Salary> optBenefits = (List<Salary>) jdbcTemplate.query(
+				OptBenefitsGetByJobRoleIdSql, new Object[] { jobRoleId },
+				new SalaryRowMapper());
+		logger.debug("Retrieved OptionalBenefits : {}", () -> optBenefits);
+		return optBenefits;
+	}
 }
---
+++ hua-server/src/main/java/com/hamdard/hua/rowmapper/SalaryRowMapper.java
@@ -0,0 +1,23 @@
+package com.hamdard.hua.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.hamdard.hua.model.Salary;
+
+public class SalaryRowMapper implements RowMapper<Salary> {
+
+	public Salary mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Salary salary = new Salary();
+		salary.setSalaryId(rs.getLong("SALARY_ID"));
+		salary.setDescription(rs.getString("DESCRIPTION"));
+		salary.setJobRoleId(rs.getLong("JOB_ROLE_ID"));
+		salary.setSalCompId(rs.getLong("SAL_COMP_ID"));
+		salary.setSalValue(rs.getDouble("SAL_VALUE"));
+		salary.setMaxAllowLimit(rs.getDouble("MAX_ALLOW_LIMIT"));
+		salary.setSalOptCompFlag(rs.getInt("SAL_OPT_COMP_FLAG"));
+		return salary;
+	}
+}
