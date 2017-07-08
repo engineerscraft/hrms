4aa6550722ea1db949917bded104710ca43899c6
Saptarshi Basu <sapb2004@gmail.com>
2017-07-08 12:56:10 +0530

    Default error page added

---
+++ hua-server/src/main/java/com/hamdard/hua/reqmap/Routing.java
@@ -0,0 +1,23 @@
+package com.hamdard.hua.reqmap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
+
+@Controller
+public class Routing {
+
+    @RequestMapping({"/resources/**"}) 
+    public String api(final HttpServletRequest request){
+        String path = (String) request.getAttribute(
+                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        return path;
+    }
+    
+    @RequestMapping({ "", "/home", "/employeeHierarchySearch", "/employeeCreation", "/forbidden", "/404" })
+    public String gui() {
+        return "forward:/index.html";
+    }
+}
