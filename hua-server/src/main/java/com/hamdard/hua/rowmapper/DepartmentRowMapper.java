88f4fb0f2bc6c08ec2ff1917e40b3fb350f5c9ff
Somdeb Samanta <samanta.som@gmail.com>
2017-07-09 23:02:27 +0530

    Added History Insertion for Emp Profile Updation along with Error Response handling

--- hua-server/src/main/java/com/hamdard/hua/rowmapper/DepartmentRowMapper.java
+++ hua-server/src/main/java/com/hamdard/hua/rowmapper/DepartmentRowMapper.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.hamdard.hua.model.Department;
-import com.hamdard.hua.model.Unit;
 
 /**
  * @author isomdeb
